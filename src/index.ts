b00b7d4a49b2f05cbde01fbb866fd11fa4370787
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-26 13:43:23 +0600

    app running

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// -------------main function-----------
 
 const main = async () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
   dotenv.config();
-  app.use("/graphql", graphqlHtt);
+  // app.use(
+  //   "/graphql",
+  //   graphqlHTTP({
+  //     schema,
+  //     graphiql: true,
+  //   })
+  // );
+
+  app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+  });
 };
 
 main().catch((error) => {
